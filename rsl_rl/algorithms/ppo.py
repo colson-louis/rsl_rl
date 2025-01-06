84da8e9fd0f521facefc78c84808e2defbd05167
colson-louis <louiscolson80@gmail.com>
2025-01-06 16:48:54 +0100

    modif ppo lr change rate with kl and networks to float64

--- rsl_rl/algorithms/ppo.py
+++ rsl_rl/algorithms/ppo.py
@@ -140,9 +140,9 @@ class PPO:
                     )
                     kl_mean = torch.mean(kl)
 
-                    if kl_mean > self.desired_kl * 2.0:
-                        self.learning_rate = max(1e-5, self.learning_rate / 1.5)
-                    elif kl_mean < self.desired_kl / 2.0 and kl_mean > 0.0:
+                    if kl_mean > self.desired_kl * 1.5:
+                        self.learning_rate = max(1e-6, self.learning_rate / 1.5)
+                    elif kl_mean < self.desired_kl / 1.5 and kl_mean > 0.0:
                         self.learning_rate = min(1e-2, self.learning_rate * 1.5)
 
                     for param_group in self.optimizer.param_groups:
